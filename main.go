f6fb8492b86f47e24f35193d941f097ca657a2d3
Kaden King <kingk45@ufl.edu>
2019-12-14 22:57:12 -0500

    added port again

--- main.go
+++ main.go
@@ -4,15 +4,24 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "hey")
 }
 
+func getPort() string {
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatal("could not find a port")
+	}
+	return ":" + port
+}
+
 func main() {
 	http.HandleFunc("/", handler)
-	err := http.ListenAndServe(":8080", nil)
+	err := http.ListenAndServe(getPort(), nil)
 	if err != nil {
 		log.Fatal(err)
 	}
